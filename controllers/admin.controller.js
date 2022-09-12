e4a900bd05611a4a94cfe0a35bf377c89934f9ac
ShahriarNiloy <shahariarniloyblv@gmail.com>
2022-09-12 15:57:12 +0600

    admin user details access

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -1,13 +1,32 @@
 const User = require("../models/user.model");
+const mongoose = require("mongoose");
 
 const getAdminUsers = async (req, res, next) => {
   try {
     const users = await User.find();
 
-    res.send(users);
+    res.render("manage-users", { users });
   } catch (error) {
     next(error);
   }
 };
 
-module.exports = { getAdminUsers };
+const getAdminSingleUser = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      req.flash("error", "Invalid Id");
+      res.redirect("/admin/users");
+      return;
+    }
+
+    const person = await User.findById(id);
+
+    res.render("profile", { person });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = { getAdminUsers, getAdminSingleUser };
--- routes/admin.route.js
+++ routes/admin.route.js
@@ -1,7 +1,11 @@
-const { getAdminUsers } = require("../controllers/admin.controller");
+const {
+  getAdminUsers,
+  getAdminSingleUser,
+} = require("../controllers/admin.controller");
 
 const adminRouter = require("express").Router();
 
 adminRouter.get("/users", getAdminUsers);
+adminRouter.get("/user/:id", getAdminSingleUser);
 
 module.exports = adminRouter;
