a057071f325daa96b4734e2c67e8e17e61da8f18
ShahriarNiloy <shahariarniloyblv@gmail.com>
2022-09-12 16:33:30 +0600

    complete project

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -1,5 +1,6 @@
 const User = require("../models/user.model");
 const mongoose = require("mongoose");
+const { roles } = require("../utils/constants");
 
 const getAdminUsers = async (req, res, next) => {
   try {
@@ -29,4 +30,39 @@ const getAdminSingleUser = async (req, res, next) => {
   }
 };
 
-module.exports = { getAdminUsers, getAdminSingleUser };
+const postAdminUpdateRole = async (req, res, next) => {
+  const { id, role } = req.body;
+
+  if (!id || !role) {
+    req.flash("error", "Invalid request");
+    return res.redirect("back");
+  }
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    req.flash("error", "Invalid Id");
+    return res.redirect("back");
+  }
+
+  const rolesArray = Object.values(roles);
+  if (!rolesArray.includes(role)) {
+    req.flash("error", "Invalid role");
+    return res.redirect("back");
+  }
+
+  if (req.user.id === id) {
+    req.flash("error", "Admins cannot remove themselves, ask another admin");
+    return res.redirect("back");
+  }
+
+  const user = await User.findByIdAndUpdate(
+    id,
+    { role },
+    { new: true, runValidators: true }
+  );
+
+  req.flash("info", `Updated role for ${user.email} to ${user.role}`);
+
+  res.redirect("back");
+};
+
+module.exports = { getAdminUsers, getAdminSingleUser, postAdminUpdateRole };
--- routes/admin.route.js
+++ routes/admin.route.js
@@ -1,11 +1,14 @@
 const {
   getAdminUsers,
   getAdminSingleUser,
+  postAdminUpdateRole,
 } = require("../controllers/admin.controller");
 
 const adminRouter = require("express").Router();
 
 adminRouter.get("/users", getAdminUsers);
 adminRouter.get("/user/:id", getAdminSingleUser);
+
+adminRouter.post("/update-role", postAdminUpdateRole);
 
 module.exports = adminRouter;
